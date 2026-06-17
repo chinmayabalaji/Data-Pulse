1904f86ddc0a8072a2c35b80ed05987d150fe005
Chinmaya-Balaji <chinmayabalaji03@gmail.com>
2026-06-17 11:34:39 +0530

    changing state lock bucket

--- Infrastructure/EC2/backend.tf
+++ Infrastructure/EC2/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "state-bucket"
+    bucket = "state-lock-bucket-s3"
     key    = "Data-Pulse/EC2/terraform.tfstate"
     region = "eu-north-1"
     use_lockfile = true
--- Infrastructure/S3/backend.tf
+++ Infrastructure/S3/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "datapulse-state-bucket"
+    bucket = "state-lock-bucket-s3"
     key    = "Data-Pulse/S3/terraform.tfstate"
     region = "eu-north-1"
     use_lockfile = true
